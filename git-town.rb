902eac44fbce8c33ed6fc86b658bbc46890f72f7
Kevin Goslar <kevin.goslar@gmail.com>
2014-06-10 07:51:47 -0700

    Version 0.2.2

--- git-town.rb
+++ git-town.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class GitTown < Formula
   homepage 'https://github.com/Originate/git-town'
-  url 'https://github.com/Originate/git-town/archive/v0.2.1.tar.gz'
-  sha1 'f2592822c821ddc9b3136a0857a433e11d2069c5'
-  version '0.2.1'
+  url 'https://github.com/Originate/git-town/archive/v0.2.2.tar.gz'
+  sha1 '23fd63d781b4449458c5be45e3be22b7cbebad87'
+  version '0.2.2'
 
   def install
     libexec.install Dir['*']
