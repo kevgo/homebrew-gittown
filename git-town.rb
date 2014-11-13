4596a9718a3254143b1cbbb28f27013a5daf7218
charlierudolph <charles.rudolph@originate.com>
2014-11-13 11:15:49 -0800

    Version 0.4

--- git-town.rb
+++ git-town.rb
@@ -2,13 +2,14 @@ require "formula"
 
 class GitTown < Formula
   homepage 'https://github.com/Originate/git-town'
-  url 'https://github.com/Originate/git-town/archive/v0.3.tar.gz'
-  sha1 'ad8b33d83feb02e5b422e335dc3a2c0e464c1790'
-  version '0.3'
+  url 'https://github.com/Originate/git-town/archive/v0.4.tar.gz'
+  sha1 'ffa10472696d29074eb2605ceb3f57ff93189ee7'
+  version '0.4'
 
   def install
     libexec.install Dir['*']
     bin.install_symlink Dir["#{libexec}/git-*"]
     bin.install_symlink "#{libexec}/helpers"
+    man1.install Dir["man/man1/*.1"]
   end
 end
