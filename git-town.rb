9aa58b6bd02d6574d0f94ac582859a205bf3e655
charlierudolph <charles.rudolph@originate.com>
2014-11-13 11:20:49 -0800

    Fix sha1 of v0.4 download

--- git-town.rb
+++ git-town.rb
@@ -3,7 +3,7 @@ require "formula"
 class GitTown < Formula
   homepage 'https://github.com/Originate/git-town'
   url 'https://github.com/Originate/git-town/archive/v0.4.tar.gz'
-  sha1 'ffa10472696d29074eb2605ceb3f57ff93189ee7'
+  sha1 'c8f8afac248402ccecfa9b868dec897b809d3be7'
   version '0.4'
 
   def install
