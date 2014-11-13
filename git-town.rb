839a3ed7254d215248e6fe15beb5e7b8dfa6f8e4
charlierudolph <charles.rudolph@originate.com>
2014-11-13 11:47:33 -0800

    man install as symlink

--- git-town.rb
+++ git-town.rb
@@ -10,6 +10,6 @@ class GitTown < Formula
     libexec.install Dir['*']
     bin.install_symlink Dir["#{libexec}/git-*"]
     bin.install_symlink "#{libexec}/helpers"
-    man1.install Dir["man/man1/*.1"]
+    man1.install_symlink Dir["#{libexec}/man/man1/*.1"]
   end
 end
