0f7be7a16603bf5f0f65cea4ab86894a70049a1a
Kevin Goslar <kevin.goslar@originate.com>
2014-12-28 10:24:05 -0800

    Remove explicit version

--- git-town.rb
+++ git-town.rb
@@ -4,7 +4,6 @@ class GitTown < Formula
   homepage 'https://github.com/Originate/git-town'
   url 'https://github.com/Originate/git-town/archive/v0.4.1.tar.gz'
   sha1 '4d089310a3368285b16f5ddb2c1bfb0b0b02a29c'
-  version '0.4.1'
 
   def install
     libexec.install Dir['*']
